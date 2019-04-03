077f54349d192e8d22d18c86f34d44faa8a14116
Truong <ledanh072346@gmail.com>
2019-04-03 12:59:33 +0700

    update index_header

--- Source/javascript/index.js
+++ Source/javascript/index.js
@@ -1,16 +1,31 @@
 
 /*menu dropdown mobile*/
+// function menuDrop(){
+// 	var menu = document.getElementById("my-nav-menu");
+// 	var y = document.getElementById("btn-menu");
+// 	if(menu.className === "nav-menu"){
+// 		menu.className += " nav-menu-mobile";
+// 		y.style.background = "#cc0000";
+// 	}else {
+// 		menu.className = "nav-menu";
+// 		y.style.background = "#000";
+// 	}
+// }
+
 function menuDrop(){
 	var menu = document.getElementById("my-nav-menu");
-	var y = document.getElementById("btn-menu");
+	// var y = document.getElementById("btn-menu");
 	if(menu.className === "nav-menu"){
 		menu.className += " nav-menu-mobile";
-		y.style.background = "#cc0000";
+		menu.style.width = "60%";
 	}else {
 		menu.className = "nav-menu";
-		y.style.background = "#000";
 	}
+
 }
+
+
+
 
 function lgDrop(){
 	document.getElementById("myDropdown").classList.toggle("show");
@@ -33,10 +48,44 @@ for(i = 0; i<x.length; i++){
 window.onscroll = function() {scrollFunc()};
 
 function scrollFunc(){
-	if(document.body.scrollTop > 140 || document.documentElement.scrollTop > 140){
+	if(document.body.scrollTop > 120 || document.documentElement.scrollTop > 120){
 		document.getElementById("scroll-header").style.top = "0";
 	}else {
 		document.getElementById("scroll-header").style.top = "-200px";
 	}
 }
 
+
+
+/*Slide show*/
+var index = 1,
+	slides = document.getElementsByClassName("slide"),
+	i;
+slideShow(index);
+
+function slideFoward (n){
+	slideShow(index += n);
+}
+
+function slideCurrent(n){
+	slideShow(index = n);
+}
+
+function slideShow(n){
+	if(n > slides.length){
+		index = 1;
+	}
+	if( n < 1){
+		index = slides.length;
+	}
+
+	for(i = 0; i < slides.length; i++){
+		slides[i].style.display = "none";
+	}
+	index++;
+	if(index > slides.length){
+		index = 1;
+	}
+	slides[index - 1].style.display = "block";
+	setTimeout(slideShow, 3000);
+}
