31819a8ec907969cd7ccb432536f1c3b62378f9a
Truong072348 <47754112+Truong072348@users.noreply.github.com>
2019-05-04 20:55:05 +0700

    update

--- Source/javascript/index.js
+++ Source/javascript/index.js
@@ -1,15 +1,4 @@
-/*menu dropdown mobile*/
-// function menuDrop(){
-// 	var menu = document.getElementById("my-nav-menu");
-// 	var y = document.getElementById("btn-menu");
-// 	if(menu.className === "nav-menu"){
-// 		menu.className += " nav-menu-mobile";
-// 		y.style.background = "#cc0000";
-// 	}else {
-// 		menu.className = "nav-menu";
-// 		y.style.background = "#000";
-// 	}
-// }
+
 
 function menuDrop() {
     var menu = document.getElementById("my-nav-menu");
@@ -58,82 +47,47 @@ function scrollFunc() {
     }
 }
 
-
-
-/*Slide show*/
-var index = 1,
-    slides = document.getElementsByClassName("slide"),
-    i;
-slideShow(index);
-
-function slideFoward(n) {
-    slideShow(index += n);
-}
-
-function slideCurrent(n) {
-    slideShow(index = n);
-}
-
-function slideShow(n) {
-    if (n > slides.length) {
-        index = 1;
-    }
-    if (n < 1) {
-        index = slides.length;
-    }
-
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    index++;
-    if (index > slides.length) {
-        index = 1;
-    }
-    slides[index - 1].style.display = "block";
-    setTimeout(slideShow, 3000);
-}
-
 /*----lightSlide------*/
-$(document).ready(function() {
-    $('#toeic, #ielts, #avgt').lightSlider({
-        adaptiveHeight: true,
-        item: 1,
-        slideMargin: 0,
-        loop: true,
-        speed: 400,
-        auto: false,
-        responsive: [{
-                breakpoint: 1400,
-                settings: {
-                    item: 4,
-                    slideMargin: 20,
-                }
-            },
-            {
-                breakpoint: 991,
-                settings: {
-                    item: 3,
-                    slideMargin: 6,
-                }
-            },
-            {
-                breakpoint: 767,
-                settings: {
-                    item: 2,
-                    slideMargin: 4,
+// $(document).ready(function() {
+//     $('#toeic, #ielts, #avgt').lightSlider({
+//         adaptiveHeight: true,
+//         item: 1,
+//         slideMargin: 0,
+//         loop: true,
+//         speed: 400,
+//         auto: false,
+//         responsive: [{
+//                 breakpoint: 1400,
+//                 settings: {
+//                     item: 4,
+//                     slideMargin: 20,
+//                 }
+//             },
+//             {
+//                 breakpoint: 991,
+//                 settings: {
+//                     item: 3,
+//                     slideMargin: 6,
+//                 }
+//             },
+//             {
+//                 breakpoint: 767,
+//                 settings: {
+//                     item: 2,
+//                     slideMargin: 4,
 
-                }
-            },
-            {
-                breakpoint: 676,
-                settings: {
-                    item: 1,
-                    slideMargin: 4,
+//                 }
+//             },
+//             {
+//                 breakpoint: 676,
+//                 settings: {
+//                     item: 1,
+//                     slideMargin: 4,
 
-                }
-            }
-        ]
-    });
-    $('.lSNext').append('<i class="fas fa-chevron-right"></i>');
-    $('.lSPrev').append('<i class="fas fa-chevron-left"></i>');
-});
+//                 }
+//             }
+//         ]
+//     });
+//     $('.lSNext').append('<i class="fas fa-chevron-right"></i>');
+//     $('.lSPrev').append('<i class="fas fa-chevron-left"></i>');
+// });
